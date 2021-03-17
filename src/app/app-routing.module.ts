02dd2204fdcafeb372801bbf00ae9db8ca640139
lakindu2002 <lakindu2002@live.com>
2021-03-17 22:02:16 +0530

    Add app logo and routing on guest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ContactUsComponent } from "./components/guest/contact-us/contact-us.component";
+import { HomeComponent } from "./components/guest/home/home.component";
 import { PageNotFoundComponent } from "./components/shared/page-not-found/page-not-found.component";
 
 const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'contactus',
+    component: ContactUsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
   {
     path: "**",
     component: PageNotFoundComponent
