2325d29621606b842055352bc8bddafd6bdad41e
lakindu2002 <lakindu2002@live.com>
2021-03-17 21:36:23 +0530

    Updated Guest Components

--- src/app/components/guest/guest.module.ts
+++ src/app/components/guest/guest.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavBarComponent, HomeComponent, ContactUsComponent, LoginComponent, SignUpComponent],
   imports: [],
   exports: []
 })
