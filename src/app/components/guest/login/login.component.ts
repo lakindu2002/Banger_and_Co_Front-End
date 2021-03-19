38a12018fab50f490c1d616fdd39c800426a686e
lakindu2002 <lakindu2002@live.com>
2021-03-20 00:34:53 +0530

    Created login form

--- src/app/components/guest/login/login.component.ts
+++ src/app/components/guest/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
@@ -8,13 +9,27 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 })
 export class LoginComponent implements OnInit {
 
+  theForm: FormGroup;
+  roleList: string[] = ["guest", "administrator"];
+
   constructor(private modalRef: BsModalRef) { }
 
   ngOnInit(): void {
+    this.theForm = new FormGroup({
+      'emailAddress': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      'selectedRole': new FormControl('guest', [Validators.required]),
+    });
   }
 
   closeModal() {
     this.modalRef.hide();
+  }
+
+  handleLogin() {
+    if (this.theForm.valid) {
+      console.log(this.theForm.value);
+    }
   }
 
 }
