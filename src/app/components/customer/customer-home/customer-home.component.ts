dfdd908a50fe38fd43be1f12500a91a8c5c6316c
lakindu2002 <lakindu2002@live.com>
2021-03-26 22:44:30 +0530

    Added a greeting message

--- src/app/components/customer/customer-home/customer-home.component.ts
+++ src/app/components/customer/customer-home/customer-home.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerHomeComponent implements OnInit {
 
+  greeting: string = "";
+  fullName: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
     document.title = "Banger and Co. - Rent a Vehicle Now";
+
+    this.generateGreeting();
+    this.fullName = "John Doe";
+  }
+
+  generateGreeting() {
+    const currentHour = new Date().getHours(); //retrieves the current hours (10pm = 22)
+    console.log(new Date().setHours(new Date().getHours()+2));
+    console.log(currentHour)
+    if (currentHour < 12) {
+      //if time is between 0am to 12pm
+      this.greeting = "Good Morning,";
+    } else if (currentHour >= 12 && currentHour < 18) {
+      //if time is between 12pm to 6pm
+      this.greeting = "Good Afternoon,";
+    } else if (currentHour >= 18 || currentHour < 24) {
+      //if time is between 6pm to 12am
+      this.greeting = "Good Evening,";
+    }
   }
 
 }
