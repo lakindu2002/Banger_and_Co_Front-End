9dfdc3dad6a64f967627bf772cdb7b52a9c6e108
lakindu2002 <lakindu2002@live.com>
2021-04-28 13:34:41 +0530

    Updated Lazy Loading

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -1,13 +1,20 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { BsDropdownModule } from "ngx-bootstrap/dropdown";
-import { SharedModule } from "../shared/shared.module";
-import { AdminHomeComponent } from "./admin-home/admin-home.component";
-import { AdminRoutingModule } from "./admin.routing.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminRoutingModule } from './admin-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { AdminNavComponent } from './admin-nav/admin-nav.component';
+
+
 
 @NgModule({
   declarations: [AdminHomeComponent, AdminNavComponent],
-  imports: [AdminRoutingModule, CommonModule, SharedModule, BsDropdownModule.forRoot()],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    SharedModule,
+    BsDropdownModule.forRoot()
+  ]
 })
 export class AdminModule { }
