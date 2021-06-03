6261e50b385e478cb97a4dec7e881dfccc4ab4ca
lakindu2002 <lakindu2002@live.com>
2021-06-03 17:15:02 +0530

    Added new route

--- src/app/components/admin/admin-vehicle-management/admin-vehicle-management.component.ts
+++ src/app/components/admin/admin-vehicle-management/admin-vehicle-management.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
+import { VehicleType } from 'src/app/models/vehicleType.model';
+import { VehicleTypeService } from 'src/app/services/vehicleType.service';
 import { VehicleCreateUpdateComponent } from './vehicle-create-update/vehicle-create-update.component';
 
 @Component({
@@ -13,13 +15,17 @@ export class AdminVehicleManagementComponent implements OnInit {
 
   modalRef: BsModalRef; //holds a reference to the opened modal
 
+  vehicleTypeList: VehicleType[] = [];
+
   constructor(
     private toast: ToastrService,
     private modalService: BsModalService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private vehicleTypeService: VehicleTypeService
   ) { }
 
   ngOnInit(): void {
+    this.getAllVehicleTypes();
   }
 
   launchCreateModal() {
@@ -27,12 +33,18 @@ export class AdminVehicleManagementComponent implements OnInit {
     this.modalRef = this.modalService.show(VehicleCreateUpdateComponent,
       //configure initial state
       {
-        animated:true,
-        ignoreBackdropClick:true,
-        keyboard:false,
-        class:'modal-lg modal-dialog-centered'
+        animated: true,
+        ignoreBackdropClick: true,
+        keyboard: false,
+        class: 'modal-lg modal-dialog-centered'
       }
     )
+  }
+
+  getAllVehicleTypes(): void {
+    this.vehicleTypeService.getAllVehicleTypes().subscribe((data) => {
+      this.vehicleTypeList = data;
+    })
   }
 
 }
