a55e31d50a00a8ab555e4ef588918c7f357408f2
lakindu2002 <lakindu2002@live.com>
2021-06-14 23:59:20 +0530

    Implemented the UI for filter

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -31,7 +31,7 @@ import { MatNativeDateModule } from "@angular/material/core";
     MatCardModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    TimepickerModule
+    TimepickerModule.forRoot()
   ],
   exports: [
     PageNotFoundComponent,
--- src/app/components/shared/vehicle-rental-filter-pop-up/vehicle-rental-filter-pop-up.component.ts
+++ src/app/components/shared/vehicle-rental-filter-pop-up/vehicle-rental-filter-pop-up.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { VehicleService } from 'src/app/services/vehicle.service';
 
 @Component({
   selector: 'app-vehicle-rental-filter-pop-up',
@@ -8,12 +11,17 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 })
 export class VehicleRentalFilterPopUpComponent implements OnInit {
 
-  constructor(private modalRef: BsModalRef) { }
+  constructor(
+    private modalRef: BsModalRef,
+    private vehicleService: VehicleService,
+    private spinner: NgxSpinnerService,
+    private toast: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
 
-  hideModal(){
+  hideModal(): void {
     this.modalRef.hide();
   }
 
