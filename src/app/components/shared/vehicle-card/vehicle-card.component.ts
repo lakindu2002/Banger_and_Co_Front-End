487eda8f1e4a4028e120d08935f8359a639ea204
lakindu2002 <lakindu2002@live.com>
2021-06-15 22:12:18 +0530

    added guest validations

--- src/app/components/shared/vehicle-card/vehicle-card.component.ts
+++ src/app/components/shared/vehicle-card/vehicle-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AuthReturn } from 'src/app/models/auth.return.model';
 import { Vehicle } from 'src/app/models/vehicle.model';
 import { LocalStorageService } from 'src/app/services/localstorage.service';
@@ -13,10 +14,14 @@ export class VehicleCardComponent implements OnInit {
   @Input('vehicleToRender') theVehicle: Vehicle; //the vehicle that will be passed into the component from a parent component.
   loggedInUser: AuthReturn;
 
-  constructor(private localStorageService: LocalStorageService) { }
+  constructor(private localStorageService: LocalStorageService, private toast: ToastrService) { }
 
   ngOnInit(): void {
     //retrieve logged in user status to enable admin only views.
     this.loggedInUser = this.localStorageService.getUserInLocalStorage();
+  }
+
+  handleGuestClick() {
+    this.toast.info("Please login or create an account inorder to rent this vehicle","Account Needed To Proceed")
   }
 }
