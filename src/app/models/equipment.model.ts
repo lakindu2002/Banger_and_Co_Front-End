526d2d040f83f7b2a2f3aa8019de5c576689f12e
Lakindu Hewawasam <lakindu2002@live.com>
2021-07-24 12:32:37 +0530

    added support for viewing total equipment cost in rental for both customer and admin

--- src/app/models/equipment.model.ts
+++ src/app/models/equipment.model.ts
@@ -6,4 +6,5 @@ export interface AdditionalEquipment {
 
   //used to indiciate add ons of the item added in rental.
   quantitySelectedForRental?: number;
+  totalCostOfEquipmentInRental?: number;
 }
