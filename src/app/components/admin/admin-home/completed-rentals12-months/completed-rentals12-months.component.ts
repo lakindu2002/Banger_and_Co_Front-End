8c8ded0cbe46a1ff52ec39655e7df2df5d7bec81
Lakindu Hewawasam <lakindu2002@live.com>
2021-08-02 21:35:03 +0530

    created the view completed rentals over past year graph

--- src/app/components/admin/admin-home/completed-rentals12-months/completed-rentals12-months.component.ts
+++ src/app/components/admin/admin-home/completed-rentals12-months/completed-rentals12-months.component.ts
@@ -8,10 +8,20 @@ import { RentalService } from 'src/app/services/rental.service';
 })
 export class CompletedRentals12MonthsComponent implements OnInit {
 
+  barChartContent: { name: string, value: number }[] = [];
+  colorScheme = {
+    domain: [
+      "#0061A8", "#2978B5", "#8AB6D6"
+    ]
+  }
+
   constructor(private rentalService: RentalService) { }
 
   ngOnInit(): void {
     this.rentalService.getCompletedRentalsForPast12Months().subscribe((data) => {
+      for (const eachMonth of data) {
+        this.barChartContent.push({ name: eachMonth.month, value: eachMonth.count });
+      }
     })
   }
 
