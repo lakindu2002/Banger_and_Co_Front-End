f2c41cea17b150609a62e54ecec7a8cace07f774
Lakindu Hewawasam <lakindu2002@live.com>
2021-09-13 22:28:51 +0530

    added max length validation on first and last name on inquiry

--- src/app/components/guest/contact-us/contact-us.component.ts
+++ src/app/components/guest/contact-us/contact-us.component.ts
@@ -28,12 +28,12 @@ export class ContactUsComponent implements OnInit {
     document.title = "Contact Us | Banger and Co."
 
     this.theForm = new FormGroup({
-      'firstName': new FormControl(null, [Validators.required, Validators.maxLength(255)]),
-      'lastName': new FormControl(null, [Validators.required, Validators.maxLength(255)]),
+      'firstName': new FormControl(null, [Validators.required, Validators.maxLength(100)]),
+      'lastName': new FormControl(null, [Validators.required, Validators.maxLength(100)]),
       'subject': new FormControl(null, [Validators.required, Validators.maxLength(1000)]),
       'message': new FormControl(null, [Validators.required, Validators.maxLength(1000)]),
       'emailAddress': new FormControl(null, [Validators.required, Validators.email, Validators.maxLength(255)]),
-      'contactNumber': new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)])
+      'contactNumber': new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]+$")])
     })
   }
 
